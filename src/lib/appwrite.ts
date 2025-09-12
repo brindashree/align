1ff0d166a05c034a57950aacb53942259960c29e
brindashree <brindashree29@gmail.com>
2025-09-12 08:19:23 +0530

    feat: appwrite setup, login, register

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,16 @@
+import "server-only";
+
+import { Client, Account, Storage, Users, Databases } from "node-appwrite";
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+    .setProject(process.env.APPWRITE_PROJECT!)
+    .setKey(process.env.APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
