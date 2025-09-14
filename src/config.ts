b47fbd889d1a4febe057747c6f3f727ca7157d05
brindashree <brindashree29@gmail.com>
2025-09-14 18:17:33 +0530

    feat: workspace & member creation

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const DATABASE_ID = process.env.APPWRITE_DATABASE_ID!;
 export const WORKSPACES_ID = process.env.APPWRITE_WORKSPACES_ID!;
 export const IMAGES_BUCKET_ID = process.env.APPWRITE_IMAGES_BUCKET_ID!;
+export const MEMBERS_ID = process.env.APPWRITE_MEMBERS_ID!;
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,16 +2,43 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspacesSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACES_ID, IMAGES_BUCKET_ID } from "@/config";
-import { ID } from "node-appwrite";
+import {
+  DATABASE_ID,
+  WORKSPACES_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+} from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
     const tablesdb = c.get("tablesdb");
+    const user = c.get("user");
+    const members = await tablesdb.listRows({
+      databaseId: DATABASE_ID,
+      tableId: MEMBERS_ID,
+      queries: [Query.equal("userId", user?.$id)],
+    });
+
+    if (members.total === 0) {
+      return c.json({
+        data: {
+          rows: [],
+          total: 0,
+        },
+      });
+    }
+    const workspacesIds = members.rows.map((member) => member.workspaceId);
 
     const workspaces = await tablesdb.listRows({
       databaseId: DATABASE_ID,
       tableId: WORKSPACES_ID,
+      queries: [
+        Query.orderDesc("$createdAt"),
+        Query.contains("$id", workspacesIds),
+      ],
     });
     return c.json({ data: workspaces });
   })
@@ -51,8 +78,21 @@ const app = new Hono()
           name,
           userId: user.$id,
           image: uploadedImageUrl,
+          inviteCode: generateInviteCode(6),
         },
       });
+
+      await tablesdb.createRow({
+        databaseId: DATABASE_ID,
+        tableId: MEMBERS_ID,
+        rowId: ID.unique(),
+        data: {
+          userId: user.$id,
+          workspaceId: workspace.$id,
+          role: MemberRole.ADMIN,
+        },
+      });
+
       return c.json({ data: workspace });
     }
   );
